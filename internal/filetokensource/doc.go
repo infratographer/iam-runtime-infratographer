8eeb1701b70c5d91ab3fc107476a362c329f7e70
Mike Mason <mimason@equinix.com>
2024-05-03 08:23:17 -0500

    update filetokensource package comment
    Co-authored-by: John Schaeffer <jschaeffer@equinix.com>
    Signed-off-by: Mike Mason <mimason@equinix.com>

--- internal/filetokensource/doc.go
+++ internal/filetokensource/doc.go
@@ -1,4 +1,4 @@
 // Package filetokensource implements the oauth2.TokenSource interface for tokens sourced from a file.
 //
-// This package can be used for sourcing Kubernetes ServiceAccount tokens to interact directly with Kube API.
+// This package can be used for sourcing Kubernetes Service Account tokens to interact directly with the Kubernetes API.
 package filetokensource
