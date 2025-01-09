dc5caa6f556c8b9b72dc3e9f6b96b78fedce6105
Mike Mason <mimason@equinix.com>
2025-01-09 11:19:43 -0600

    configure jwks refresh interval
    Currently jwks set is only fetched at startup.
    This configures the jwks storage to refresh the key set.
    
    By default, the keys are freshed every hour.
    
    Signed-off-by: Mike Mason <mimason@equinix.com>

--- internal/jwt/config.go
+++ internal/jwt/config.go
@@ -1,14 +1,17 @@
 package jwt
 
 import (
+	"time"
+
 	"github.com/spf13/pflag"
 )
 
 // Config represents the configuration for a JWT validator.
 type Config struct {
-	Disable bool
-	Issuer  string
-	JWKSURI string
+	Disable             bool
+	Issuer              string
+	JWKSURI             string
+	JWKSRefreshInterval time.Duration
 }
 
 // AddFlags sets the command line flags for JWT validation.
@@ -16,4 +19,5 @@ func AddFlags(flags *pflag.FlagSet) {
 	flags.Bool("jwt.disable", false, "Disable JWT service")
 	flags.String("jwt.issuer", "", "Issuer to use for JWT validation")
 	flags.String("jwt.jwksuri", "", "JWKS URI to use for JWT validation")
+	flags.Duration("jwt.jwksrefreshinterval", time.Hour, "sets the jwks refresh interval")
 }
